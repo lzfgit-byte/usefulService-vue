82a3591c3a524e2385046376783158dd53786ffa
lzfgit-byte <1807409742@qq.com>
2023-02-15 22:35:03 +0800

    feat:共享文本开发

--- src/const/const-data.ts
+++ src/const/const-data.ts
@@ -1,2 +1,2 @@
-export const prefix = '/api';
+export const prefix = process.env.NODE_ENV === 'development' ? '/api' : '';
 export const downloadFilePath = 'http://192.168.0.103:8080/file/downloadFile';
